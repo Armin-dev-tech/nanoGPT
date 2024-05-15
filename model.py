d5d1b6c0c148483a107e145c4a1467dae55b763a
Armin-dev-tech <arminbozorgi.ab@gmail.com>
2024-05-15 08:31:46 -0700

    passing n_embd and n_head in command line

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ https://github.com/openai/gpt-2/blob/master/src/model.py
 https://github.com/huggingface/transformers/blob/main/src/transformers/models/gpt2/modeling_gpt2.py
 """
 
+import sys
 import math
 import inspect
 from dataclasses import dataclass
@@ -110,8 +111,8 @@ class GPTConfig:
     block_size: int = 1024
     vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
     n_layer: int = 12
-    n_head: int = 12
-    n_embd: int = 768
+    n_head: argv[2] # int = 12
+    n_embd: argv[1] # int = 768
     dropout: float = 0.0
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
 
